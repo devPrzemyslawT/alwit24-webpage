54f07809226921abd1227e8ae3bc89f8c7acd376
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-03-22 17:44:36 +0100

    footer improvement

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -6,16 +6,16 @@ import logoImg from "../../../assets/img/LogoAlwit.png";
 const Footer = () => {
 	return (
 		<div className={classes.footer}>
-			<div>
+			<div className={classes.footerAdress}>
 				<p>ALWIT Krzysztof Witkowski</p>
 				<p>ul. Wł. Broniewskiego 10</p>
 				<p>09-230 Bielsk</p>
 				<p>NIP: 7742883814</p>
 			</div>
 			<div>
-				<Image height={40} width={100} alt='alwit-logo' src={logoImg} />
+				<Image height={60} width={150} alt='alwit-logo' src={logoImg} />
 			</div>
-			<div>
+			<div className={classes.footerContact}>
 				<p>E-mail: alwit@onet.pl</p>
 				<p>Tel.: +48 503-155-108</p>
 			</div>
